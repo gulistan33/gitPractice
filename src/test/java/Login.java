a10529984dd1d243aa44247edd911db664502aad
gulistan33 <gulistan33@gmail.com>
2022-01-10 02:04:31 -0800

    from repo1

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -2,6 +2,7 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("logging in");
         System.out.println("adding new line from github");
+        System.out.println("doing changes from repo1");
 
         //hello comment
     }
